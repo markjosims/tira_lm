b353a1eefb8de2fed12c7b556c3303bbdabb2227
Mark J Simmons <mjsimmons@ucsd.edu>
2026-03-04 10:12:45 -0800

    reorganize scripts; save sentences as CSV

--- scripts/data_preparation/make_sentence_list_from_hf.py
+++ scripts/data_preparation/make_sentence_list_from_hf.py
@@ -1,27 +1,39 @@
 from datasets import load_dataset
+import os
+import pandas as pd
 
 dataset_uri = 'tira-parsing/tira-parsing'
-sentences_list = 'data/sentences.txt'
+sentences_list = 'data/sentences.csv'
 
 def main():
     print(f'Loading dataset from: {dataset_uri}')
     ds = load_dataset(dataset_uri)
-    lines = []
+    rows = []
     for split in ['train', 'test', 'validation']:
         print(f'Processing split: {split}')
 
         def make_sentence_list(example, i):
             sentence = example['orig_text']
             translation = example['translation']
-            lines.append(','.join([sentence, translation, split, str(i)]))
+            rows.append({
+                'sentence': sentence,
+                'translation': translation,
+                'split': split,
+                'index': str(i)
+            })
 
             return None
 
         ds[split].map(make_sentence_list, with_indices=True)
 
     print(f'Writing sentences to: {sentences_list}')
-    with open(sentences_list, 'w') as f:
-        f.write('\n'.join(lines))
+    os.makedirs(
+        os.path.dirname(sentences_list),
+        exist_ok=True,
+    )
+    df = pd.DataFrame(rows)
+    df=df.set_index('index')
+    df.to_csv(sentences_list, index=True)
 
 if __name__ == '__main__':
     main()
