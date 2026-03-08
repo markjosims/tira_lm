08d3354cb4e190d5568889c07a846fa74ebce922
Mark <mjsimmons@ucsd.edu>
2026-03-07 19:32:03 -0800

    update filepaths

--- scripts/constants.py
+++ scripts/constants.py
@@ -1,3 +1,4 @@
-wordlist = 'doc/keywords.csv'
-edited_wordlist = 'data/edited_keywords.csv'
+wordlist = 'doc/abx_word_seeds.csv'
+edited_wordlist = 'data/edited_abx_words.csv'
 abx_wordlist = 'data/abx_words.csv'
+abx_sentence_list = 'data/abx_sentences.csv'
