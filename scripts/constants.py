f96a9096966c6f8703cce89746df0190a6c2c679
Mark <mjsimmons@ucsd.edu>
2026-03-10 11:08:08 -0700

    use absolute paths

--- scripts/constants.py
+++ scripts/constants.py
@@ -1,12 +1,16 @@
+import os
+import torch
+
 # filepaths
-seed_words = 'doc/abx_word_seeds.csv'
-edited_wordlist = 'data/edited_abx_words.csv'
+seed_words = os.path.abspath('doc/abx_word_seeds.csv')
+edited_wordlist = os.path.abspath('data/edited_abx_words.csv')
 
-documentation_dir = 'doc/'
-frame_config = 'doc/frames.yaml'
-frame_list = 'data/abx_frames.csv'
+documentation_dir = os.path.abspath('doc/')
+frame_config = os.path.abspath('doc/frames.yaml')
+frame_list = os.path.abspath('data/abx_frames.csv')
 
-abx_sentence_list = 'data/abx_sentences.csv'
+abx_sentence_list = os.path.abspath('data/abx_sentences.csv')
 
 # misc
 random_seed = 42
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
