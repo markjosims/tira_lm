ffc6dd7115cd412988d180af303eb416965331ad
Mark <mjsimmons@ucsd.edu>
2026-03-11 11:06:59 -0700

    fix deleting in wrong scope

--- scripts/inference/word_embeddings.py
+++ scripts/inference/word_embeddings.py
@@ -105,7 +105,6 @@ def get_batch_embeddings(model, tokenizer, batch):
     for item in ['a', 'b', 'x']:
         sentence = 'sentence_' + item
         sentence_embeddings = get_encoder_outputs(model, batch[sentence]['input_ids'])
-        del outputs
         batch_embeddings = []
         batch_size = batch[sentence]['input_ids'].shape[0]
         for i in range(batch_size):
