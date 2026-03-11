788c56e3ced46601478a7a23f086fd8a0e1be69e
Mark <mjsimmons@ucsd.edu>
2026-03-11 13:46:29 -0700

    get paths from project root

--- scripts/constants.py
+++ scripts/constants.py
@@ -1,20 +1,22 @@
 import os
 import torch
 
-# filepaths
-seed_words = os.path.abspath('doc/abx_word_seeds.csv')
-edited_wordlist = os.path.abspath('data/edited_abx_words.csv')
+project_root = os.path.dirname(os.path.dirname(__file__))
 
-documentation_dir = os.path.abspath('doc/')
-frame_config = os.path.abspath('doc/frames.yaml')
-frame_list = os.path.abspath('data/abx_frames.csv')
+# filepaths
+seed_words = os.path.join(project_root, 'doc/abx_word_seeds.csv')
+edited_wordlist = os.path.join(project_root, 'data/edited_abx_words.csv')
+
+documentation_dir = os.path.join(project_root, 'doc/')
+frame_config = os.path.join(project_root, 'doc/frames.yaml')
+frame_list = os.path.join(project_root, 'data/abx_frames.csv')
 
 dataset_uri = 'tira-parsing/tira-parsing'
-abx_sentence_list = os.path.abspath('data/abx_sentences.csv')
-tira_sentence_list = os.path.abspath('data/tira_sentences.csv')
-tira_word_list = os.path.abspath('data/tira_words.csv')
-word2sentence_list = os.path.abspath('data/word2sentence.csv')
-parenthetical_list = os.path.abspath('doc/parentheticals.tsv')
+abx_sentence_list = os.path.join(project_root, 'data/abx_sentences.csv')
+tira_sentence_list = os.path.join(project_root, 'data/tira_sentences.csv')
+tira_word_list = os.path.join(project_root, 'data/tira_words.csv')
+word2sentence_list = os.path.join(project_root, 'data/word2sentence.csv')
+parenthetical_list = os.path.join(project_root, 'doc/parentheticals.tsv')
 
 # misc
 random_seed = os.environ.get('RANDOM_SEED', 42)
